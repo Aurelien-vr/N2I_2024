a7f800e63f56044d94bccad8d7f0926f6cb8a881
Mallart <matheo.allart09@gmail.com>
2024-12-05 23:36:22 +0100

    add title

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="./styles/dark.css">
 </head>
 <body>
-
+    <h1 class="title">Bienvenue sur <span class="i">float</span>.</h1>
 <script type="module">
     import { Navbar } from "./scripts/generators/navbar.js";
     let navbar = new Navbar([
